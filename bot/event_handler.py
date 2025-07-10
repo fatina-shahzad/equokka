8c9ee0a16e5b2d13825dbcfd97e514f65c9fdbd6
Shazil Ahmed <shazil.ahmed@emumba.com>
2025-07-10 16:26:48 +0500

    added mindmaps integration

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -4,6 +4,7 @@ import re
 import logging
 from slack_sdk.errors import SlackApiError
 from slack_sdk.socket_mode.response import SocketModeResponse
+from bot.utils import get_s3_presigned_url
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +24,37 @@ class EventHandler:
             if event_type == "message" and "bot_id" not in event:
                 text = event.get("text", "").lower()
                 channel_id = event.get("channel")
+
+                # Look for mindap requests
+                if text == "leave request procedure":
+                    html_url = get_s3_presigned_url(bucket_name="equokka-mindmaps-poc", region="us-west-2", file_name="emumba_leave_mindmap.html", expiration=3000)
+                    await client.web_client.chat_postMessage(
+                        channel=event.get("channel"),
+                        text=f"View here: <{html_url}|Leave guidelines>"
+                    )
+                elif text == "loan policy":
+                    html_url = get_s3_presigned_url(bucket_name="equokka-mindmaps-poc", region="us-west-2", file_name="emumba_leave_mindmap.html", expiration=3000)
+                    await client.web_client.chat_postMessage(
+                        channel=event.get("channel"),
+                        text=f"View here: <{html_url}|Loan Request Policy>"
+                    )
+                elif text == "day care":
+                    html_url = get_s3_presigned_url(bucket_name="equokka-mindmaps-poc", region="us-west-2", file_name="emumba_leave_mindmap.html", expiration=3000)
+                    await client.web_client.chat_postMessage(
+                        channel=event.get("channel"),
+                        text=f"View here: <{html_url}|Day Care Policy>"
+                    )
+                elif text == "slack guidelines":
+                    html_url = get_s3_presigned_url(bucket_name="equokka-mindmaps-poc", region="us-west-2", file_name="emumba_leave_mindmap.html", expiration=3000)
+                    await client.web_client.chat_postMessage(
+                        channel=event.get("channel"),
+                        text=f"View here: <{html_url}|Slack Guidelines>"
+                    )    
+                else:
+                    await client.web_client.chat_postMessage(
+                        channel=event.get("channel"),
+                        text="Thanks for your message!"
+                    )
 
                 # Look for a pattern like "podcast on XYZ"
                 match = re.search(r"podcast (?:on|about)\s+(.*)", text)
